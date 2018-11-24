fb445a361f129b995891bafd1c123864dffb79ba
wanghan <419024902@qq.com>
2018-11-24 18:27:00 +0800

    添加bbb

--- git_test/git/src/main/java/cn/bbb.java
+++ git_test/git/src/main/java/cn/bbb.java
@@ -1,8 +1,7 @@
 package cn;
 
-public class aaa {
+public class bbb {
     public static void main(String[] args) {
-        System.out.println("111111111111");
-
+        System.out.println("BBBBBBBBBB");
     }
 }
